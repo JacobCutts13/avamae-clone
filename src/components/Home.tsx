fbf256736f95aead154d6d8cf1dcc2d8d7263c91
unknown <jacobdcutts@gmail.com>
2022-06-17 11:10:35 +0100

    formatting

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,30 +3,31 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BannerImage } from "../interfaces";
 
-
 export default function Home(): JSX.Element {
-
   const [bannerImages, setBannerImages] = useState<BannerImage[]>([]);
 
   useEffect(() => {
     const getImages = async () => {
       try {
-        const apiImages = await axios.get("https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details");
+        const apiImages = await axios.get(
+          "https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details"
+        );
         if (apiImages.data?.Details) {
-          setBannerImages(apiImages.data.Details)
+          setBannerImages(apiImages.data.Details);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
     getImages();
-  }, [])
+  }, []);
   return (
-    <div id="home">Home
+    <div id="home">
+      Home
       <div className="image-banner">
         <div className="image-banner-images">
           {bannerImages.length > 0 &&
-            bannerImages.map(bannerImage => (
+            bannerImages.map((bannerImage) => (
               <div key={bannerImage.title}>
                 <img src={bannerImage.url} alt={bannerImage.title} />
               </div>
@@ -40,11 +41,18 @@ export default function Home(): JSX.Element {
           </Link>
         </div>
       </div>
-
       <div className="learn-more">
         <div className="learn-more-text">
           <h1>Sed non nunc non nibh luctus condimentum</h1>
-          <p>In eu elementum orci. Praesent eu nunc a lacus aliquam laoreet sit amet ut metus. Nam facilisis est libero, vitae ullamcorper orci fermentum faucibus. Vivamus faucibus suscipit lorem. Ut ac dapibus magna. Fusce porta ullamcorper quam vel congue. Nullam quis nulla est. Nullam est erat, imperdiet quis egestas vel, lobortis quis odio. Phasellus maximus semper tempor. Etiam rutrum rhoncus mauris sit amet aliquam.</p>
+          <p>
+            In eu elementum orci. Praesent eu nunc a lacus aliquam laoreet sit
+            amet ut metus. Nam facilisis est libero, vitae ullamcorper orci
+            fermentum faucibus. Vivamus faucibus suscipit lorem. Ut ac dapibus
+            magna. Fusce porta ullamcorper quam vel congue. Nullam quis nulla
+            est. Nullam est erat, imperdiet quis egestas vel, lobortis quis
+            odio. Phasellus maximus semper tempor. Etiam rutrum rhoncus mauris
+            sit amet aliquam.
+          </p>
           <li>Aliquam mattis lacus non dui fermentum blandit.</li>
           <li>Interdum et malesuada fames ac ante ipsum primis in faucibus.</li>
           <li>Duis ornare justo quis aliquet dictum.</li>
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,5 @@
-
 export interface BannerImage {
-    title: string;
-    subtitle: string;
-    url: string;
+  title: string;
+  subtitle: string;
+  url: string;
 }
